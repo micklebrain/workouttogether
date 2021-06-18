6c4c1b13c58129909d7d51c5af44382c8779253c
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-06-18 00:11:12 -0400

    initial;

---
+++ WorkoutTogether/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkoutTogether
+//
+//  Created by Nathan Nguyen on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Let's workout together")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkoutTogether/WorkoutTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTogetherApp.swift
+//  WorkoutTogether
+//
+//  Created by Nathan Nguyen on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
